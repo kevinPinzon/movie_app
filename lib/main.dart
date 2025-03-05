b9e4436521a5289a863b45a2d527d84064b0a124
kevinPinzon <149128073+kevinPinzon30@users.noreply.github.com>
2025-03-05 00:55:42 -0600

    config dependency injection with GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:movie_app/core/routes/navigation.dart';
+import 'package:movie_app/feature/movies/data/repositories/movie_repository.dart';
+import 'package:movie_app/feature/movies/domain/repositories/movie_repository.dart';
+import 'package:movie_app/feature/movies/presentation/blocs/movie_bloc.dart';
+import 'package:movie_app/core/network/server_api_client.dart';
+import 'package:http/http.dart' as http;
+import 'package:movie_app/core/network/network_info.dart';
+
+final GetIt getIt = GetIt.instance;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  getIt
+    ..registerLazySingleton(() => http.Client())
+    ..registerLazySingleton<NetworkInfoRepository>(
+        () => NetworkInfoRepositoryImpl())
+    ..registerLazySingleton<ServerApiClient>(
+        () => ServerApiClient(networkInfoRepository: getIt()))
+    ..registerLazySingleton<MovieRepository>(() => MovieRepositoryImpl(
+          serverApiClient: getIt(),
+          networkInfoRepository: getIt(),
+        ))
+    ..registerFactory(() => MovieBloc(movieRepository: getIt()));
+
   runApp(App());
 }
 
@@ -13,9 +36,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      routerConfig: _appRouter.router,
+    return RepositoryProvider<MovieRepository>(
+      create: (context) => getIt<MovieRepository>(),
+      child: BlocProvider(
+        create: (context) => MovieBloc(
+          movieRepository: RepositoryProvider.of<MovieRepository>(context),
+        ),
+        child: MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          routerConfig: _appRouter.router,
+        ),
+      ),
     );
   }
 }
